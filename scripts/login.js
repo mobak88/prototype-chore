722eaee20e60ea70e1a90d88138b7a1056023179
mobak88 <78964465+mobak88@users.noreply.github.com>
2021-11-18 11:53:25 +0100

    Creating the rest of the pages

--- scripts/login.js
+++ scripts/login.js
@@ -5,19 +5,28 @@ const loginSelect = document.getElementById('rolle');
 const loginBtn = document.getElementById('login-btn');
 
 const chooseRole = () => {
-    if (loginSelect.value === 'Forelder') {
-        console.log('Forelder');
-        loginForm.action = '/overview.html';
-    }
-    if (loginSelect.value === 'Barn') {
-        console.log('Barn');
-        loginForm.action = '/overview.html';
-    }
+  if (loginSelect.value === 'Forelder') {
+    console.log('Forelder');
+    loginForm.action = '/overview.html';
+    loginSelect.classList.remove('role-error');
+  }
+  if (loginSelect.value === 'Barn') {
+    console.log('Barn');
+    loginForm.action = '/overview.html';
+    loginSelect.classList.remove('role-error');
+  }
+  if (loginSelect.value === 'Velg rolle') {
+    loginSelect.classList.add('role-error');
+  }
 };
 
+loginSelect.addEventListener('change', () => {
+  chooseRole();
+});
+
 loginForm.addEventListener('submit', (e) => {
-    if (loginSelect.value === 'Velg rolle') {
-        e.preventDefault();
-    }
-    chooseRole();
+  if (loginSelect.value === 'Velg rolle') {
+    e.preventDefault();
+  }
+  chooseRole();
 });
